9c4578d3be3b2e6848e00bbede5efe57b3f5e3b9
Chupin Daniil <chupindaniil@gmail.com>
2021-01-28 12:44:29 +0700

    Initial Commit

---
+++ HW_2_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2_01
+//
+//  Created by Даниил on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
